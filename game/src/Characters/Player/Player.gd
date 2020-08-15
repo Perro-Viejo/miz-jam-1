692fd26597931537da0bf20a133c5d9451c9d108
José Miguel Romero Espitia <jmromeroe@gmail.com>
2020-08-15 14:21:26 -0500

    Adding initial movement for boat

--- game/src/Characters/Player/Player.gd
+++ game/src/Characters/Player/Player.gd
@@ -1,52 +1,25 @@
 class_name Player
 extends KinematicBody2D
-# ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ Variables ░░░░
-export(Color) var dialog_color
 
 const STATES = {
-	WALK = 'Walk',
+	MOVE = 'Move',
 	IDLE = 'Idle',
-	GRAB = 'Grab',
-	DROP = 'Drop',
-	FISH = 'Fish',
-	TALK = 'Talk'
+	DEAD = 'Dead'
 }
-
-var is_moving = false
-var is_out: bool = false
-var node_to_interact: Pickable = null setget _set_node_to_interact
-var grabbing: bool = false
-var on_ground: bool = false
-var fishing: bool = false
-var fs_id: String = 'FS_Dirt'
-var foot = 'L'
-var is_paused := false
 
 onready var cam: Camera2D = $Camera2D
 onready var sprite: AnimatedSprite = $AnimatedSprite
-onready var fishing_spot: ColorRect = $FishingSpot
-onready var foot_area: Area2D = $FootArea
-# ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ Funciones ░░░░
+
+export(float) var speed = 30
+export(float) var acceleration = 0.05
+export(float) var deceleration = 0.01
+var motion = Vector2.ZERO
+var velocity = Vector2.ZERO
+
 func _ready() -> void:
-	
-	fishing_spot._fish_splash = $FishSplash
-	
-	# Escuchar eventos de los hijos de satán
-	$FootArea.connect('body_entered', self, 'toggle_on_ground', [ true ])
-	$FootArea.connect('body_exited', self, 'toggle_on_ground')
-	$AnimatedSprite.connect('frame_changed', self, '_on_frame_changed')
-
-	# Conectarse a eventos del universo
-	Event.connect('line_triggered', self, '_should_speak')
-	Event.connect('control_toggled', self, '_toggle_control')
-
-	# Definir estado por defecto
 	play_animation()
 
-
 func change_zoom(out: bool = true) -> void:
-	is_out = out
-
 	if out:
 		Event.emit_signal('play_requested', 'UI', 'ZoomOut')
 	else:
@@ -65,34 +38,14 @@ func change_zoom(out: bool = true) -> void:
 	$Tween.start()
 
 	yield($Tween, 'tween_completed')
-
+	
+func _physics_process(delta):
+	motion = move_and_slide(motion)
 
 func play_animation(state: String = '') -> void:
 	match state:
 		_:
 			sprite.play('Idle')
-
-
-func toggle_on_ground(body: Node2D, on: = false) -> void:
-	if not body.is_in_group('Floor'): return
-
-
-func _on_frame_changed() -> void:
-	if $AnimatedSprite.animation == 'Run' \
-		or $AnimatedSprite.animation == 'RunGrab':
-		match $AnimatedSprite.frame:
-			0,2:
-				play_fs(fs_id)
-				if fs_id == 'FS_Water':
-					match foot:
-						'L':
-							$Splash_L.set_emitting(true)
-							$Splash_R.restart()
-							foot = 'R'
-						'R':
-							$Splash_R.set_emitting(true)
-							$Splash_L.restart()
-							foot = 'L'
 
 
 func play_fs(id):
@@ -103,11 +56,6 @@ func speak(text := '', time_to_disappear := 0):
 	Event.emit_signal('character_spoke', self, text, time_to_disappear)
 
 
-# Sirve para disparar comportamientos cuando se ha completado un diálogo
-#func spoke():
-#	pass
-
-
 func _should_speak(character_name, text, time, emotion) -> void:
 	if name.to_lower() == character_name:
 		speak(text, time)
@@ -116,13 +64,3 @@ func _should_speak(character_name, text, time, emotion) -> void:
 
 func _toggle_control() -> void:
 	$StateMachine.transition_to(STATES.IDLE)
-	is_paused = !is_paused
-
-
-func _set_node_to_interact(new_node: Pickable) -> void:
-	if node_to_interact:
-		node_to_interact.hide_interaction()
-	if new_node and new_node.is_in_group('Pickable'):
-		new_node.show_interaction()
-
-	node_to_interact = new_node
--- game/src/Characters/Player/States/Move.gd
+++ game/src/Characters/Player/States/Move.gd
@@ -1,89 +1,23 @@
 extends "res://src/StateMachine/State.gd"
 # ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ Variables ░░░░
-export(float) var speed = 2
+export var rotate_speed = 1
 
-var dir:Vector2 = Vector2.ZERO
-var can_move = true
-
-var _last_dir: Vector2 = Vector2.ZERO
-
-onready var _calc_speed: float = speed * 60
-onready var _owner: Player = owner
-# ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ Funciones ░░░░
 func _ready():
 	Event.connect('dialog_event', self, '_on_dialog_event')
 
-
-func _unhandled_input(event: InputEvent) -> void:
-	if _owner.is_paused: return
-
-	if event.is_action_pressed('Grab'):
-		if _owner.fishing:
-			_owner.fishing_spot.pull_fish()
-		if _owner.node_to_interact and not _owner.grabbing:
-			_state_machine.transition_to(_owner.STATES.GRAB)
-	elif event.is_action_pressed('Drop'):
-		if _owner.node_to_interact:
-			if _owner.grabbing:
-				_state_machine.transition_to(_owner.STATES.DROP, { dir = _last_dir })
-			elif _owner.node_to_interact.dialog:
-				Event.emit_signal('dialog_requested', _owner.node_to_interact.dialog)
-		else:
-			if _owner.fishing:
-				_owner.fishing_spot.switch_bait()
-
-	if event.is_action_pressed('Fish'):
-		if _owner.fishing:
-			_state_machine.transition_to(_owner.STATES.IDLE)
-		else:
-			if not _owner.grabbing and not _owner.is_moving:
-				_state_machine.transition_to(owner.STATES.FISH)
-
-
 func _physics_process(delta) -> void:
-	if not can_move or _owner.is_paused:
-		return
-
-	dir.x = Input.get_action_strength("Right") - Input.get_action_strength("Left")
-	dir.y = Input.get_action_strength("Down") - Input.get_action_strength("Up")
-
-	if dir.x != 0:
-		_last_dir.x = dir.x
-		_last_dir.y = 0
-
-		_owner.sprite.flip_h = dir.x < 0
-#		
-
-	elif dir.y != 0:
-		_last_dir.x = 0
-		_last_dir.y = dir.y
-		
-
-	if not _owner.is_out:
-		_owner.move_and_collide(dir * _calc_speed * delta)
+	if Input.is_action_pressed("Right"):
+		owner.rotation += rotate_speed*delta
+			
+	if Input.is_action_pressed("Left"):
+		owner.rotation -= rotate_speed*delta
+	
+	owner.velocity = Vector2(owner.speed, 0).rotated(owner.rotation - PI/2)
+	if Input.is_action_pressed("Up"):
+		owner.motion = owner.motion.linear_interpolate(owner.velocity, owner.acceleration)
 	else:
-		_owner.move_and_collide(dir * _calc_speed * 2 * delta)
-
-	if not dir == Vector2(0,0) and not _owner.is_moving:
-		_state_machine.transition_to(owner.STATES.WALK)
-	elif dir == Vector2(0,0) and _owner.is_moving:
-		_state_machine.transition_to(owner.STATES.IDLE)
-
-	if _owner.grabbing and _owner.node_to_interact:
-		_owner.node_to_interact.global_position = _owner.global_position
-		_owner.node_to_interact.position.y -= 7
-
-func _on_dialog_event(playing, countdown, duration):
-	if playing:
-		yield(get_tree().create_timer(countdown), 'timeout')
-		_state_machine.transition_to(_owner.STATES.IDLE)
-		toggle_movement()
-		yield(get_tree().create_timer(duration), 'timeout')
-		toggle_movement()
-
-func toggle_movement():
-	can_move = !can_move
-
+		owner.motion = owner.motion.linear_interpolate(Vector2.ZERO, owner.deceleration)
+		
 func enter(msg: Dictionary = {}) -> void:
 	.enter(msg)
 
