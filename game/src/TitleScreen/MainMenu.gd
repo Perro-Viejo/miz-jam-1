d6470f0f5818b7c3a9d4bf678e8a66fb531f5cbd
Carenalga <mapedorr@gmail.com>
2020-08-17 06:28:27 -0500

    Updated transition to credits animation.
    Refactored Credits layout and node names.
    Updated Perro Viejo logo to match the game color palette.

--- game/src/TitleScreen/MainMenu.gd
+++ game/src/TitleScreen/MainMenu.gd
@@ -10,16 +10,16 @@ onready var _name_container: VBoxContainer = find_node('NameContainer')
 onready var _buttons_container: HBoxContainer = find_node('ButtonsContainer')
 onready var _new_game: Button = find_node('NewGame')
 onready var _options: Button = find_node('Options')
-onready var _credits: Button = find_node('Credits')
+onready var _credits_btn: Button = find_node('CreditsBtn')
 onready var _exit: Button = find_node('Exit')
-onready var _credits_container: TextureButton = find_node('CreditsContainer')
-onready var _credits_back: Button = _credits_container.get_node('Back')
+onready var _credits: MarginContainer = find_node('Credits')
+onready var _credits_back: Button = _credits.find_node('Back')
 onready var _devs: Label = find_node('Devs')
 # ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ Funciones ░░░░
 func _ready()->void:
 	Event.MainMenu = true
 	guiBrain.gui_collect_focusgroup()
-	_credits_container.hide()
+	_credits.hide()
 
 	if Settings.HTML5:
 		_exit.visible = false
@@ -27,7 +27,7 @@ func _ready()->void:
 	# Conectarse a señales de los hijos
 	_new_game.connect('pressed', self, '_on_NewGame_pressed')
 	_options.connect('pressed', self, '_on_Options_pressed')
-	_credits.connect('pressed', self, '_on_Credits_pressed')
+	_credits_btn.connect('pressed', self, '_on_Credits_pressed')
 	_exit.connect('pressed', self, '_on_Exit_pressed')
 
 	# Conectarse a señales del universo pokémon
@@ -61,16 +61,17 @@ func _on_Options_pressed()->void:
 
 func _on_Credits_pressed() -> void:
 	_is_credits = !_is_credits
-	_credits_container.visible = _is_credits
-	_devs.visible = _is_credits
-	_name_container.visible = !_is_credits
-	_buttons_container.visible = !_is_credits
+	if _is_credits:
+		$AnimationPlayer.play('show_credits')
+	else:
+		$AnimationPlayer.play('credits_exit')
+	yield($AnimationPlayer, 'animation_finished')
 	if _is_credits:
 		_credits_back.connect('pressed', self, '_on_Credits_pressed')
 		_credits_back.grab_focus()
 	else:
 		_credits_back.disconnect('pressed', self, '_on_Credits_pressed')
-		_credits.grab_focus()
+		_credits_btn.grab_focus()
 
 
 func _on_Exit_pressed()->void:
@@ -80,6 +81,6 @@ func _on_Exit_pressed()->void:
 func retranslate()->void:
 	_new_game.text = tr('NEW_GAME')
 	_options.text = tr('OPTIONS')
-	_credits.text = tr('CREDITS')
+	_credits_btn.text = tr('CREDITS')
 	_exit.text = tr('EXIT')
 	_credits_back.text = tr('BACK')
