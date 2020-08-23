689fd061c4cb5dade5ce5d2f7ce9d483ac24e89c
Carenalga <mapedorr@gmail.com>
2020-08-23 13:34:45 -0500

    Fixed bugs in level restart for cops.
    Updated the desing of level 3.
    Level 2 now goes to Level 3.

--- game/src/GUI/HUD/Timer/CopTimer.gd
+++ game/src/GUI/HUD/Timer/CopTimer.gd
@@ -2,15 +2,22 @@ extends Node2D
 class_name CopTimer
 
 export(float) var timer_seconds = 10
-onready var _tween = $Cop.get_node("Tween")
+onready var _tween: Tween = $Cop.get_node("Tween")
 
 func _ready():
 	$Cop.global_position = $StartPoint.global_position
 	$Cop.get_node("AnimatedSprite").play("default")
 	
+	# Conectarse a eventos de los hijos
+	if not _tween.is_connected("tween_completed", self, "timeout"):
+		_tween.connect("tween_completed", self, "timeout")
+	
+	# Conectarse a eventos del universo chimpokomón
+	if not Event.is_connected('player_killed', self, '_stop'):
+		Event.connect('player_killed', self, '_stop')
+
 func start():
 	timer_seconds = Data.get_data(Data.LEVEL_TIME)
-	_tween.connect("tween_completed", self, "timeout")
 	_tween.interpolate_property(
 		$Cop, 
 		"global_position:x", 
@@ -19,6 +26,11 @@ func start():
 		timer_seconds)
 	_tween.start()
 	_ready()
-	
+
+
 func timeout(obj, key):
 	Event.emit_signal("target_deployed", $EndPoint)
+
+
+func _stop() -> void:
+	_tween.remove_all()
